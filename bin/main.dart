253be8ce9c9425bb47e697867d3584d03293ae23
Fernando Trigoso <fertrig@gmail.com>
2019-04-15 14:29:51 -0400

    asserting in main

--- bin/main.dart
+++ bin/main.dart
@@ -4,6 +4,7 @@ import '../lib/build_main.dart';
 main(List<String> arguments) {
   reflectable_main.initializeReflectable();
 
+  assert(reflector.annotatedClasses.length == 2);
   print('length ${reflector.annotatedClasses.length}');
   reflector.annotatedClasses.forEach((cls) {
     print("Annotated class ${cls.qualifiedName}");
